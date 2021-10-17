e61df6ed77b358494ffcc2fa09d06334a7d2cd14
Reindorius_Desktop_Home <conquistadore@126.com>
2021-10-17 18:39:02 +0800

    complete authentication

--- flask-pro/authentication.py
+++ flask-pro/authentication.py
@@ -1,14 +1,14 @@
 from functools import wraps
 
-from flask import jsonify
+from flask import jsonify, request
 
 
 def check_auth(username, password):
     return username == 'admin' and password == 'secret'
 
 
-def authenticate():
-    message = {'message': 'Authenticated'}
+def not_authenticated():
+    message = {'message': 'Not Authenticated'}
     resp = jsonify(message)
 
     resp.status_code = 401
@@ -17,3 +17,15 @@ def authenticate():
     return resp
 
 
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth:
+            return not_authenticated()
+        elif not check_auth(auth.username, auth.password):
+            return not_authenticated()
+        else:
+            return f(*args, **kwargs)
+
+    return decorated
